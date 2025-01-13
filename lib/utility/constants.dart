f952d8b74ac951f6ac059cbe4358d82a20269d0c
subash240603 <130847228+subash240603@users.noreply.github.com>
2025-01-13 11:18:16 +0530

    pushing

---
+++ lib/utility/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Light-themed pastel colors inspired by the uploaded UI design
+const primaryColor = Color.fromARGB(255, 66, 11, 231); // Purple pastel
+const secondaryColor = Color.fromARGB(255, 45, 97, 114); // Orange pastel
+const accentColor1 = Color.fromARGB(161, 7, 240, 197); // Teal pastel
+const accentColor2 = Color(0xFFFF647C); // Coral pastel
+const bgColor = Color.fromARGB(255, 207, 224, 190); // Light cream background
+const cardColor =
+    Color.fromARGB(255, 84, 63, 166); // White for cards and containersr
+const textColor = Color.fromARGB(255, 0, 0, 0); // Dark gray for text
+const defaultPadding = 16.0;
+
+const MAIN_URL = 'http://localhost:3000';
